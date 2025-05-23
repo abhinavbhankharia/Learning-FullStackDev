c82bbe8476ac28812548e2503c684f3174d6f922
abhinavbhankharia <abhinavbhankharia@gmail.com>
2025-05-23 23:14:19 +0530

    Understamding about healthchecks and add it to the project

--- Backend/Project/VidTube/src/app.js
+++ Backend/Project/VidTube/src/app.js
@@ -10,9 +10,17 @@ app.use(
     
 )
 
+
 //common middleware
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+
+//import routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+
+//routes
+app.use("/api/v1/healthCheck", healthCheckRouter)
 
 export { app }
