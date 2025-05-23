cc136f3fb73d54a51ced0804e56be00606b7e9ea
abhinavbhankharia <abhinavbhankharia@gmail.com>
2025-05-23 22:17:37 +0530

    Standardizing API response and error handler

---
+++ Backend/Project/VidTube/src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+//Error response and API response are always in the class format
+
+class ApiResponse{
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+export { ApiResponse }
